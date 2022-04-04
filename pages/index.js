a8a76bad723b47ead2fd97e1118b1479f8abe425
William Felker <gndclouds@icloud.com>
2022-04-04 22:46:43 +0000

    place holder text

--- pages/index.js
+++ pages/index.js
@@ -68,16 +68,16 @@ const products = [
 export default function Index() {
   return (
     <>
-      <div className="bg-white">Hero</div>
-      <div className="bg-white">Search</div>
+      {/* <div className="bg-white">Hero</div>
+      <div className="bg-white">Search</div> */}
       <div className="bg-white">
         <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
           <h2 className="text-2xl font-extrabold tracking-tight text-gray-900">
-            Customers also purchased
+            Earth API Coming Soon
           </h2>
 
           <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-            {products.map((product) => (
+            {/* {products.map((product) => (
               <div key={product.id} className="group relative">
                 <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
                   <div className="w-full h-full object-center object-cover lg:w-full lg:h-full">
@@ -106,7 +106,7 @@ export default function Index() {
                   </p>
                 </div>
               </div>
-            ))}
+            ))} */}
           </div>
         </div>
       </div>
