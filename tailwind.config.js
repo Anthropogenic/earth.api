bcdaf23a94f462459094c37250a63bd938c43d2b
William Felker <gndclouds@icloud.com>
2022-04-27 18:57:24 +0000

    added @tailwindcss/typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,5 +6,8 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/aspect-ratio")],
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/typography"),
+  ],
 };
