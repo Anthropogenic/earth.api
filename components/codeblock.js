079408a0a3ddc50adb5528e8526fd7bbd941e062
William Felker <gndclouds@icloud.com>
2022-05-25 00:52:12 +0000

    init

--- components/codeblock.js
+++ components/codeblock.js
@@ -1,14 +1,14 @@
 import Link from "next/link";
 import Image from "next/image";
 import { useState } from "react";
-
+import Highlight from "react-highlight";
 import Select from "react-select";
 
 import { Menu, Transition } from "@headlessui/react";
 import { ChevronDownIcon } from "@heroicons/react/solid";
 
 import GlitchLogo from "../public/GlitchLogo.png";
-import CodePenLogo from "../public/CodepenLogo.png";
+import CodeSandboxLogo from "../public/CodeSandboxLogo.png";
 import OtherLogo from "../public/UnknownLogo.png";
 import GitHubLogo from "../public/GitHubLogo.png";
 
@@ -40,58 +40,61 @@ const ExamplesToggleData = [
     input: "This is the input",
     output: "This is the output",
   },
-  {
-    title: "co2",
-    state: "active",
-    demoLinks: [
-      { title: "Glitch", href: "/" },
-      { title: "CodePen", href: "/" },
-      { title: "Other", href: "/" },
-      { title: "Github", href: "/" },
-    ],
-    input: "This is the input",
-    output: "This is the output",
-  },
-  {
-    title: "n2o",
-    state: "inactive",
-    demoLinks: [
-      { title: "Glitch", href: "/" },
-      { title: "CodePen", href: "/" },
-      { title: "Other", href: "/" },
-      { title: "Github", href: "/" },
-    ],
-    input: "This is the input",
-    output: "This is the output",
-  },
-  {
-    title: "sf6",
-    state: "inactive",
-    demoLinks: [
-      { title: "Glitch", href: "/" },
-      { title: "CodePen", href: "/" },
-      { title: "Other", href: "/" },
-      { title: "Github", href: "/" },
-    ],
-    input: "This is the input",
-    output: "This is the output",
-  },
-];
-const actions = [
-  { label: "Add", value: 1 },
-  { label: "Edit", value: 2 },
-  { label: "Delete", value: 3 },
 ];
 
-export default function CodeBlock({ data }) {
-  const [value, setValue] = useState("");
-
+export default function CodeBlock() {
+  const text =
+    "Lorem ipsum dolor sit amet, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
   return (
     <>
-      <div className="col-md-4">
-        <Select options={actions} onChange={setValue} />
+      <div className="col-md-4 bg-[#111E35] border-2 border-[#111E35] rounded-lg">
+        <div className="border-b-2 border-[#111E35]">
+          <div className="flex flex-wrap relative font-medium">
+            <div className="p-4">/api/co2/2002</div>
+            <div className="m-2 p-2 border-2 border-[#585f6e] rounded uppercase absolute right-0">
+              co<sub>2</sub>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-wrap border-2 border-[#17253D]">
+          <div className="w-full sm:w-1/2">
+            <pre>code</pre>
+          </div>
+          <div className="w-full sm:w-1/2">
+            <pre>code code code</pre>
+            <pre>code code code</pre>
+            <pre>code code code</pre>
+            <pre>code code code</pre>
+            <pre>code code code</pre>
+            <pre>code code code</pre>
+          </div>
+        </div>
+        <div className="flex flex-row">
+          <div className="m-2 p-2 bg-[#3695C4] rounded">
+            <button
+              onClick={() => {
+                navigator.clipboard.writeText(text);
+              }}
+            >
+              Copy to Clipboard
+            </button>
+          </div>
+          <div className="">
+            <div className="flex flex-wrap">
+              <Link href="https://glitch.com/@anthropogenic/earth-api-examples">
+                <a className="w-4">
+                  <Image src={GlitchLogo} alt="Glitch Example Code" />
+                </a>
+              </Link>
+              <Link href="/blog/hello-world">
+                <a className="w-4">
+                  <Image src={CodeSandboxLogo} alt="Picture of the author" />
+                </a>
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="col-md-4">{setValue.value}</div>
     </>
   );
 }
