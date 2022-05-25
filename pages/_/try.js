9ce1e87fbc8107e567ac9e5229f8f9af501dc58b
William Felker <gndclouds@icloud.com>
2022-05-25 00:53:17 +0000

    Demo of try ti comp

---
+++ pages/_/try.js
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import Layout from "../../components/layout";
+import CodeBlock from "../../components/codeblock"
+
+
+export default function Try() {
+  return (
+    <Layout>
+      <div className="h-screen">
+      <CodeBlock/>
+      </div>
+    </Layout>
+  );
+} 
+
