4780002eaa18af7d687ec19b254427ac60679129
William Felker <gndclouds@icloud.com>
2022-06-10 02:16:07 +0000

    Really it works now

--- pages/co2/index.js
+++ pages/co2/index.js
@@ -13,6 +13,8 @@ const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export default function Index() {
   const { data, error } = useSWR("/api/co2/yearly", fetcher);
+  const { asPath } = useRouter();
+  const DataRouteURL = asPath.substring(1);
 
   if (error) return <div>Failed to load</div>;
   if (!data) return <div>Loading...</div>;
