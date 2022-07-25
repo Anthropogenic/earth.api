46a0506fc11a1ed3a9f7de8d1a9849329ad42a71
William Felker <gndclouds@icloud.com>
2022-07-25 19:28:48 +0000

    init for ch₄

---
+++ pages/api/ch4/[id].js
@@ -0,0 +1,22 @@
+import { ch4Yearly } from "../../../data/ch4";
+
+export default function carbonHandler({ query: { id } }, res) {
+  // const filtered = co2Yearly.filter((c) => c.id === id);
+  // const yearlySource = data[0].source[0];
+  // const yearlyData = data[1].data[0];
+  const filtered = ch4Yearly.filter((c) => c.id === id);
+
+  // const filtered = co2Yearly.filter(function (element) {
+  //     return element.subElements.some( function (subElement) {
+  //         return subElement.surname === surname
+  //     });
+  // });
+
+  if (filtered.length > 0) {
+    res.status(200).json(filtered[0]);
+  } else {
+    res.status(404).json({ message: `Yearly co2 data for: ${id} not found.` });
+
+    // res.status(404).json({ message: id });
+  }
+}
