ad4bf574671b648d7970bdca5237ea850b00e600
William Felker <gndclouds@icloud.com>
2022-07-26 16:55:40 +0000

    removed tabs

--- pages/ch4/index.js
+++ pages/ch4/index.js
@@ -35,13 +35,6 @@ export default function Index() {
           <div className="mt-8 flex flex-col">
             <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
               <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-                <DataTableTabs
-                  YearHref=""
-                  MonthHref="/ch4/monthly"
-                  WeekHref=""
-                  DayHref=""
-                />
-
                 <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
                   <EmissionsTable
                     key={data}
