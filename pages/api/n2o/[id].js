3b702a781cfe7039cd2d07a20e858a94098e4bf3
William Felker <gndclouds@icloud.com>
2022-08-03 16:26:05 +0000

    concat of multi db search added

--- pages/api/n2o/[id].js
+++ pages/api/n2o/[id].js
@@ -1,12 +1,13 @@
-import { n2oYearly } from "../../../data/n2o";
+import { n2oMonthly, n2oYearly } from "../../../data/n2o";
 
 export default function carbonHandler({ query: { id } }, res) {
-  const filtered = n2oYearly.filter((c) => c.id === id);
+  let filtered = n2oYearly.concat(n2oMonthly);
 
-  // User with id exists
   if (filtered.length > 0) {
     res.status(200).json(filtered[0]);
   } else {
-    res.status(404).json({ message: `User with year: ${id} not found.` });
+    res.status(404).json({ message: `SF₆ data for: ${id} not found.` });
+    res.status(404).json({ message: `N₂O data for: ${id} not found.` });
+    // res.status(404).json({ message: id });
   }
 }
--- pages/api/sf6/[id].js
+++ pages/api/sf6/[id].js
@@ -1,11 +1,13 @@
-import { ch4Monthly } from "../../../data/ch4";
+import { sf6Monthly, sf6Yearly } from "../../../data/sf6";
 
 export default function carbonHandler({ query: { id } }, res) {
-  const filtered = ch4Monthly.filter((c) => c.id === id);
+  let filtered = sf6Yearly.concat(sf6Monthly);
 
   if (filtered.length > 0) {
     res.status(200).json(filtered[0]);
   } else {
-    res.status(404).json({ message: `CH₄ data for: ${id} not found.` });
+    res.status(404).json({ message: `SF₆ data for: ${id} not found.` });
+
+    // res.status(404).json({ message: id });
   }
 }
