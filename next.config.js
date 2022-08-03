e8f95273d08133f227d976176b0aeedf24ae3109
William Felker <gndclouds@icloud.com>
2022-08-03 16:43:14 +0000

    new doc url

--- next.config.js
+++ next.config.js
@@ -10,8 +10,7 @@ module.exports = {
     return [
       {
         source: "/docs",
-        destination:
-          "https://anthropogenic.notion.site/Earth-API-Docs-f612e44da33d44ada672295d3db45da2",
+        destination: "https://docs.hge.earth",
         permanent: true,
       },
     ];
