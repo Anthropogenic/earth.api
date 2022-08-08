0ec28c1b407ba6f63326531fba51eb6ad789c605
William Felker <gndclouds@icloud.com>
2022-08-08 15:56:04 +0000

    prisma updates

---
+++ pages/_/prisma.js
@@ -0,0 +1,14 @@
+export const getStaticProps: GetStaticProps = async () => {
+  const feed = await prisma.post.findMany({
+    where: { published: true },
+    include: {
+      author: {
+        select: { name: true },
+      },
+    },
+  });
+  return {
+    props: { feed },
+    revalidate: 10,
+  };
+};
